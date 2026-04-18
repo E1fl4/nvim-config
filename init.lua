058e4a97a12991e94c6a3b73a5ea116e2c5df002
EirikErKulere <eirikgf08@gmail.com>
2026-04-18 14:55:29 +0200

    Neotree 👍

--- init.lua
+++ init.lua
@@ -59,6 +59,16 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 require('lazy').setup({
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+  {
     'windwp/nvim-autopairs',
     config = function()
       require('nvim-autopairs').setup {}
